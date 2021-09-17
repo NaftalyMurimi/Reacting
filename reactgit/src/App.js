8b5a19d8312e4df72ca149d8fe80c3b06cfd9fab
Naftaly <80479583+NaftalyMurimi@users.noreply.github.com>
2021-09-17 09:36:34 +0300

    newfiles

--- reactgit/src/App.js
+++ reactgit/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState}from 'react'
+import './App.css'
 function App() {
+
+  const [IsRed, setRed] = useState(false);
+  const [Count, setCount] = useState(0);
+  const [Size, setSize] = useState(false)
+  const [Background, setBackground] = useState(false)
+
+  function red() {
+    setRed(!IsRed);
+    setBackground(!Background); 
+  }
+
+  function font() {
+    setSize(!Size)
+  }
+  function background() {
+    setBackground(!Background);    
+  }
+  function increment() {
+    setCount(Count+1);    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hello">
+    <h1 className={IsRed ? "red": " "}>Change My Color, Font and Background</h1>
+    <h1 className={Size ? "font": " "}>Change My Color, Font and Background</h1>
+    <h1 className={Background ? "back": " "}>Change My Color, Font and Background</h1>
+    <div className="list">
+    <input type="checkbox" onClick={red}/>Red 
+    <input type="checkbox" onClick={font}/>Font
+    <input type="checkbox" onClick={background}/> Background
     </div>
-  );
+    <button onClick={red}>Click Me!!</button>
+    <button onClick={increment}>Click Me!!</button>
+    <h1>{Count}</h1>
+      
+    </div>
+  )
 }
 
-export default App;
+export default App
