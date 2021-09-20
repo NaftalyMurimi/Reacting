9a0ff930b1f05726ddb78c730a54ed99067b7606
Naftaly <80479583+NaftalyMurimi@users.noreply.github.com>
2021-09-20 16:53:54 +0300

    starRating

--- reactgit/src/App.js
+++ reactgit/src/App.js
@@ -1,42 +1,14 @@
-import React, {useState}from 'react'
-import './App.css'
+import React from 'react';
+import Star from './Star.js';
+import State from './State.js';
+
 function App() {
-
-  const [IsRed, setRed] = useState(false);
-  const [Count, setCount] = useState(0);
-  const [Size, setSize] = useState(false)
-  const [Background, setBackground] = useState(false)
-
-  function red() {
-    setRed(!IsRed);
-    setBackground(!Background); 
-  }
-
-  function font() {
-    setSize(!Size)
-  }
-  function background() {
-    setBackground(!Background);    
-  }
-  function increment() {
-    setCount(Count+1);    
-  }
   return (
-    <div className="hello">
-    <h1 className={IsRed ? "red": " "}>Change My Color, Font and Background</h1>
-    <h1 className={Size ? "font": " "}>Change My Color, Font and Background</h1>
-    <h1 className={Background ? "back": " "}>Change My Color, Font and Background</h1>
-    <div className="list">
-    <input type="checkbox" onClick={red}/>Red 
-    <input type="checkbox" onClick={font}/>Font
-    <input type="checkbox" onClick={background}/> Background
+    <div>
+      <State/>
+      <Star/>
     </div>
-    <button onClick={red}>Click Me!!</button>
-    <button onClick={increment}>Click Me!!</button>
-    <h1>{Count}</h1>
-      
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
